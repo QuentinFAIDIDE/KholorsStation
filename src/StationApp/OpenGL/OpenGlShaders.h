a6023d89b2e7c52d1067971b43c6ac4034e209c9
Quentin Faidide <quentin.faidide@gmail.com>
2024-07-04 21:13:38 +0300

    feat: add 3x3 GPU convolution of textures

--- src/StationApp/OpenGL/OpenGlShaders.h
+++ src/StationApp/OpenGL/OpenGlShaders.h
@@ -33,10 +33,74 @@ in vec2 TexCoord;
 
 uniform sampler2D sfftTexture;
 
+// Define kernels
+#define identity mat3(0, 0, 0, 0, 1, 0, 0, 0, 0)
+#define edge0 mat3(1, 0, -1, 0, 0, 0, -1, 0, 1)
+#define edge1 mat3(0, 1, 0, 1, -4, 1, 0, 1, 0)
+#define edge2 mat3(-1, -1, -1, -1, 8, -1, -1, -1, -1)
+#define sharpen mat3(0, -1, 0, -1, 5, -1, 0, -1, 0)
+#define box_blur mat3(1, 1, 1, 1, 1, 1, 1, 1, 1) * 0.1111
+#define gaussian_blur mat3(1, 2, 1, 2, 4, 2, 1, 2, 1) * 0.0625
+#define emboss mat3(-2, -1, 0, -1, 1, 1, 0, 1, 2)
+
+// Find coordinate of matrix element from index
+vec2 kpos(int index)
+{
+    return vec2[9] (
+        vec2(-1, -1), vec2(0, -1), vec2(1, -1),
+        vec2(-1, 0), vec2(0, 0), vec2(1, 0), 
+        vec2(-1, 1), vec2(0, 1), vec2(1, 1)
+    )[index] / vec2(64, 512).xy;
+}
+
+// Extract region of dimension 3x3 from sampler centered in uv
+// sampler : texture sampler
+// uv : current coordinates on sampler
+// return : a mat3 with intensities
+mat3 region3x3(sampler2D sampler, vec2 uv)
+{
+    // Create each pixels for region
+    vec4[9] region;
+    
+    for (int i = 0; i < 9; i++)
+        region[i] = texture(sampler, uv + kpos(i));
+
+    // Create 3x3 region
+    mat3 mRegion;
+    
+    mRegion = mat3(
+        region[0][3], region[1][3], region[2][3],
+        region[3][3], region[4][3], region[5][3],
+        region[6][3], region[7][3], region[8][3]
+    );
+    
+    return mRegion;
+}
+
+// Convolve a texture with kernel
+// kernel : kernel used for convolution
+// sampler : texture sampler
+// uv : current coordinates on sampler
+float convolution(mat3 kernel, sampler2D sampler, vec2 uv)
+{
+    float fragment;
+    
+    // Extract a 3x3 region centered in uv
+    mat3 region = region3x3(sampler, uv);
+    
+    // component wise multiplication of kernel by region
+    mat3 c = matrixCompMult(kernel, region);
+    // add each component of matrix
+    float r = c[0][0] + c[1][0] + c[2][0]
+            + c[0][1] + c[1][1] + c[2][1]
+            + c[0][2] + c[1][2] + c[2][2];
+    return r;
+}
+
 void main()
 {
-    vec4 sampleWaveform = texture(sfftTexture, TexCoord);
-    FragColor = vec4(ourColor.x, ourColor.y, ourColor.z, sampleWaveform.a);
+    float intensity = convolution(emboss, sfftTexture, TexCoord);
+    FragColor = vec4(ourColor.x, ourColor.y, ourColor.z, intensity);
 }
 )";
 
