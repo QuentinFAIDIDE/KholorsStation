d1d403114cca20256f2672a5591b483c7b5706ae
Quentin Faidide <quentin.faidide@gmail.com>
2024-07-05 20:50:58 +0300

    implement more of the TrackList label detection

--- src/StationApp/GUI/TrackList.cpp
+++ src/StationApp/GUI/TrackList.cpp
@@ -4,10 +4,13 @@
 #include "StationApp/GUI/FftDrawingBackend.h"
 #include <limits>
 #include <memory>
+#include <mutex>
+#include <spdlog/spdlog.h>
 
 TrackList::TrackList()
 {
     tilesRingBuffer.reserve(TILES_RING_BUFFER_SIZE);
+    nextTileIndex = 0;
 }
 
 void TrackList::paint(juce::Graphics &g)
@@ -69,17 +72,144 @@ void TrackList::recordSfft(std::shared_ptr<NewFftDataTask> newSffts)
             {
                 continue;
             }
+            std::lock_guard lock(tilesMutex);
             // if the tile does not exist, create it
             size_t tileIndex = getOrCreateTile(j);
             //   if track not recorded in tile
-            int64_t trackIndexInTile = getTrackIndexInTile(tileIndex, newSffts->trackIdentifier);
+            int64_t trackIndexInTile = getTrackIndexInTileOrInsert(tileIndex, newSffts->trackIdentifier);
             if (trackIndexInTile < 0)
             {
                 // this tile is full, we need to abort :'(
+                spdlog::warn("Aborted recording track activity in tile because a one-second tile had more than maximum "
+                             "number of tracks active.");
                 continue;
             }
-            // TODO: add tile summed intensities to the right channel(s)
-            // TODO: add tile average freq to the right channel(s)
+            // add tile summed intensities to the channel(s)
+            // add tile average freq to the channel(s)
+            // note for summing to average that if we got previous count, we have:
+            // previous avg value A = (1/n) + sum_n(a_i)
+            // if new value is a_n+1
+            // new avg value B = (1/(n+1)) + sum_n+1(a_i) = (n/n+1)*A + (1/(n+1))*a_n+1
+            // We will use that to keep it averaged so that channel with different
+            // number of FFT per left/right channels can be compared on the spot safely and efficiently.
+
+            if (newSffts->totalNoChannels == 1 || newSffts->channelIndex == 0)
+            {
+                tilesRingBuffer[tileIndex].summedLeftChanIntensities[trackIndexInTile] += dBsum;
+                float n = float(tilesRingBuffer[tileIndex].leftChanFftCount[trackIndexInTile]);
+                tilesRingBuffer[tileIndex].averageLeftChanFrequencies[trackIndexInTile] =
+                    ((1.0f / (n + 1.0f)) * avgFreq) +
+                    ((n / (n + 1.0f)) * tilesRingBuffer[tileIndex].averageLeftChanFrequencies[trackIndexInTile]);
+                tilesRingBuffer[tileIndex].leftChanFftCount[trackIndexInTile] += 1;
+            }
+
+            if (newSffts->totalNoChannels == 1 || newSffts->channelIndex == 1)
+            {
+                tilesRingBuffer[tileIndex].summedRightChanIntensities[trackIndexInTile] += dBsum;
+                float n = float(tilesRingBuffer[tileIndex].rightChanFftCount[trackIndexInTile]);
+                tilesRingBuffer[tileIndex].averageRightChanFrequencies[trackIndexInTile] =
+                    ((1.0f / (n + 1.0f)) * avgFreq) +
+                    ((n / (n + 1.0f)) * tilesRingBuffer[tileIndex].averageRightChanFrequencies[trackIndexInTile]);
+                tilesRingBuffer[tileIndex].rightChanFftCount[trackIndexInTile] += 1;
+            }
         }
     }
+}
+
+size_t TrackList::getOrCreateTile(int64_t tilePosition)
+{
+    auto existingTile = indexOfTilesWithData.find(tilePosition);
+    if (existingTile != indexOfTilesWithData.end())
+    {
+        return existingTile->second;
+    }
+    else
+    {
+        size_t newTileIndex = nextTileIndex;
+
+        if (tilesRingBuffer.size() < TILES_RING_BUFFER_SIZE)
+        {
+            // ring buffer has not yet reached its full size
+            // we will extend it
+            tilesRingBuffer.emplace_back();
+        }
+
+        // clear the previous (replaced) tile from the tile index
+        indexOfTilesWithData.erase(tilesRingBuffer[newTileIndex].tilePosition);
+        indexOfTilesWithData[tilePosition] = newTileIndex;
+
+        // clear all values from the tile
+        tilesRingBuffer[newTileIndex].tilePosition = tilePosition;
+        tilesRingBuffer[newTileIndex].numSeenTracks = 0;
+        for (size_t i = 0; i < MAX_TRACKS_PER_TILE; i++)
+        {
+            tilesRingBuffer[newTileIndex].summedLeftChanIntensities[i] = 0.0f;
+            tilesRingBuffer[newTileIndex].summedRightChanIntensities[i] = 0.0f;
+            tilesRingBuffer[newTileIndex].averageLeftChanFrequencies[i] = 0.0f;
+            tilesRingBuffer[newTileIndex].averageRightChanFrequencies[i] = 0.0f;
+            tilesRingBuffer[newTileIndex].leftChanFftCount[i] = 0;
+            tilesRingBuffer[newTileIndex].rightChanFftCount[i] = 0;
+        }
+
+        nextTileIndex++;
+        if (nextTileIndex >= TILES_RING_BUFFER_SIZE)
+        {
+            nextTileIndex = 0;
+        }
+
+        return newTileIndex;
+    }
+}
+
+int64_t TrackList::getTrackIndexInTileOrInsert(size_t tileIndex, uint64_t trackIdentifier)
+{
+    // first step is to search for the track in the tile
+    for (size_t i = 0; i < tilesRingBuffer[tileIndex].numSeenTracks; i++)
+    {
+        if (tilesRingBuffer[tileIndex].trackIdentifiers[i] == trackIdentifier)
+        {
+            return (int64_t)i;
+        }
+    }
+    // if we reach here, the track was not found
+    // if there is no more space abort with -1
+    if (tilesRingBuffer[tileIndex].numSeenTracks == MAX_TRACKS_PER_TILE)
+    {
+        return -1;
+    }
+    // otherwise we insert it
+    size_t newTrackIndex = tilesRingBuffer[tileIndex].numSeenTracks;
+    tilesRingBuffer[tileIndex].trackIdentifiers[newTrackIndex] = trackIdentifier;
+    tilesRingBuffer[tileIndex].numSeenTracks += 1;
+
+    return (int64_t)newTrackIndex;
+}
+
+void TrackList::clear()
+{
+    std::lock_guard lock(tilesMutex);
+    for (size_t i = 0; i < tilesRingBuffer.size(); i++)
+    {
+        tilesRingBuffer[i].numSeenTracks = 0;
+    }
+}
+
+std::vector<TrackList::TrackPresenceSummary> TrackList::getVisibleTracks(int64_t startTile, int64_t endTile)
+{
+    std::vector<TrackPresenceSummary> response;
+    std::map<int64_t, size_t> existingTracks;
+    for (int64_t currentTile = startTile; currentTile <= endTile; currentTile++)
+    {
+        // if the tile exist, get its index, otherwise skip
+        // for each track in the tile
+        //   if not existing in response array yet, create an entry
+        //   add left channel volume, remove right channel ones
+        //   add total of both channels
+        //   sum average frequencies and append to noFreqAvg
+    }
+    // for each track
+    //   compute final stereo balance
+    //   divide and set avgFreq
+
+    return response;
 }
--- src/StationApp/GUI/TrackList.h
+++ src/StationApp/GUI/TrackList.h
@@ -33,8 +33,25 @@ class TrackList : public juce::Component
         float averageRightChanFrequencies[MAX_TRACKS_PER_TILE];
         size_t leftChanFftCount[MAX_TRACKS_PER_TILE];
         size_t rightChanFftCount[MAX_TRACKS_PER_TILE];
-        int numSeenTracks;    /**< How many track are used in there */
+        size_t numSeenTracks; /**< How many track are used in there */
         int64_t tilePosition; /**< Position of the tile in the track (in seconds starting at 0) */
+    };
+
+    /**
+     * @brief Summarises information about a track
+     * (average freq, stereo balance)
+     * about an audio segment.
+     */
+    struct TrackPresenceSummary
+    {
+        uint64_t trackIdentifier;  /**< identifier of the track */
+        int stereoBalance;         /**< 0 for left, 1 for right, 2 for both */
+        float allChannel;          /** used for computing stereoBalance */
+        float stereoDiff;          /** used for computing stereo balance */
+        float avgFreq;             /**< average frequency on the stereo chan */
+        float noFreqsAveraged;     /**< Used for computing avgFreq */
+        float leftChannelAvgFreq;  /**< used for avgFreq once balance is known */
+        float rightChannelAvgFreq; /**< used for avgFreq once balance is known */
     };
 
     /**
@@ -54,6 +71,7 @@ class TrackList : public juce::Component
   private:
     /**
      * @brief Get tile index, and create it if not exists.
+     * To be called when you already locked the tileMutex.
      *
      * @param tilePosition tile index in seconds starting at zero.
      * @return size_t index of the tile in the ring buffer.
@@ -64,14 +82,27 @@ class TrackList : public juce::Component
      * @brief Get index of the track inside the tile, eventually
      * creating it, will return -1 if no more space for tracks in this
      * tile.
+     * Note that you NEEED to have first retrieved the tile index from its
+     * position in getOrCreateTile.
+     * To be called when you already locked the tileMutex.
      *
      * @param tileIndex index of the tile in the ring buffer
      * @param trackIdentifier track identifier to add.
      * @return index of the tile, -1 if no more room
      */
-    int64_t getTrackIndexInTile(size_t tileIndex, uint64_t trackIdentifier);
+    int64_t getTrackIndexInTileOrInsert(size_t tileIndex, uint64_t trackIdentifier);
+
+    /**
+     * @brief Get a vector of visible track on this section with summarized info.
+     *
+     * @param startTile tile position of the first tile
+     * @param endTile tile position of the last tile
+     * @return std::vector<TrackPresenceSummary>
+     */
+    std::vector<TrackPresenceSummary> getVisibleTracks(int64_t startTile, int64_t endTile);
 
     std::vector<TracksPlaybackInfoTile> tilesRingBuffer; /**< ring buffer of tiles track playback info */
     std::map<int64_t, size_t> indexOfTilesWithData;      /**< tile indexes indexes by tile position in seconds */
+    size_t nextTileIndex;                                /**< index of the next tile to create/replace */
     std::mutex tilesMutex;
 };
