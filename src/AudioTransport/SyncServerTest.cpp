2769f8d312013a9321ac30c05539cee9f942af48
Quentin Faidide <quentin.faidide@gmail.com>
2024-10-13 02:40:27 +0300

    chore: fix Linux build

--- src/AudioTransport/SyncServerTest.cpp
+++ src/AudioTransport/SyncServerTest.cpp
@@ -19,7 +19,7 @@ void SyncServerTestSuite::smokeTest01()
     SyncServer server;
 
     auto state = server.setServerToListenOnPort(8879);
-    if (state != NO_ERROR)
+    if (state != ServerStartError::SERV_NO_ERROR)
     {
         throw std::runtime_error("Unable to start gRPC server");
     }
@@ -43,7 +43,7 @@ void SyncServerTestSuite::smokeTest01()
         throw std::runtime_error("Server state is not running");
     }
     state = server.setServerToListenOnPort(8899);
-    if (state != NO_ERROR)
+    if (state != ServerStartError::SERV_NO_ERROR)
     {
         throw std::runtime_error("Unable to start gRPC server");
     }
@@ -57,7 +57,7 @@ void SyncServerTestSuite::smokeTest01()
         throw std::runtime_error("Server state is running");
     }
     state = server.setServerToListenOnPort(8899);
-    if (state != NO_ERROR)
+    if (state != ServerStartError::SERV_NO_ERROR)
     {
         throw std::runtime_error("Unable to start gRPC server");
     }
